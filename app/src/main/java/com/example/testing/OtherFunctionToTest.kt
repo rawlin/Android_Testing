37f46581cee78bccc97b688b45b502f78cfafd8e
rawlin <rawlincrasto@gmail.com>
2020-11-04 22:18:15 +0530

    Testing Android Components(contexts) & @Before,@After

--- app/src/main/java/com/example/testing/OtherFunctionToTest.kt
+++ app/src/main/java/com/example/testing/OtherFunctionToTest.kt
@@ -15,7 +15,7 @@ object OtherFunctionToTest {
         var a = 0L
         var b = 1L
         var c = 1L
-        (1..n-2).forEach { i ->
+        (1..n-1).forEach { i ->
             c = a + b
             a = b
             b = c
