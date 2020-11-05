9cec3e83afb00da2a3e15bda3ce97d20be8e9b0c
rawlin <rawlincrasto@gmail.com>
2020-11-05 11:36:58 +0530

    setting up Hilt

---
+++ ShoppingListTestingYT/app/src/main/java/com/androiddevs/shoppinglisttestingyt/data/remote/PixabayAPI.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.shoppinglisttestingyt.data.remote
+
+import com.androiddevs.shoppinglisttestingyt.BuildConfig
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixabayAPI {
+
+    @GET("/api/")
+    suspend fun searchForImage(
+        @Query("q") searchQery:String,
+        @Query("key") apikey:String =BuildConfig.API_KEY
+    ):Response<ImageResponse>
+
+}
