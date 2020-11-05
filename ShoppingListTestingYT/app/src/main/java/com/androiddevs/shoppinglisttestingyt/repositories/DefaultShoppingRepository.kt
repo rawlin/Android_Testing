fdb064c72b685b2babac03cf1a1c622020d33a92
rawlin <rawlincrasto@gmail.com>
2020-11-05 12:28:14 +0530

    created fake repository

---
+++ ShoppingListTestingYT/app/src/main/java/com/androiddevs/shoppinglisttestingyt/repositories/DefaultShoppingRepository.kt
@@ -0,0 +1,50 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+import retrofit2.Response
+import java.lang.Exception
+import javax.inject.Inject
+
+class DefaultShoppingRepository
+@Inject
+constructor(
+    private val shoppingDao: ShoppingDao,
+    private val pixabayAPI: PixabayAPI
+):ShoppingRepository {
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.insertShoppingItem(shoppingItem)
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.deleteShoppingItem(shoppingItem)
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return shoppingDao.observeAllShoppingItems()
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return shoppingDao.observeTotalPrice()
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return try {
+            val response=pixabayAPI.searchForImage(imageQuery)
+            if (response.isSuccessful){
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?: Resource.error("An error occurred",null)
+            } else{
+                Resource.error("An unknown error occurred",null)
+            }
+        }catch (e:Exception){
+            Resource.error("Couldn't reach the server.Check Internet connection",null)
+        }
+    }
+}
