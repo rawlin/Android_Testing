d1cf396b09f41286228b5378d552c33d56d5a554
rawlin <rawlincrasto@gmail.com>
2020-11-06 23:57:06 +0530

    Testing with Dagger-Hilt

--- ShoppingListTestingYT/app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaoTest.kt
+++ ShoppingListTestingYT/app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaoTest.kt
@@ -7,6 +7,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
@@ -15,67 +17,69 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaoTest {
 
     @get:Rule
-    var instantTaskExecuterRule=InstantTaskExecutorRule()
+    var hiltRule = HiltAndroidRule(this)
 
-    private lateinit var database: ShoppingItemDatabase
-    private lateinit var dao:ShoppingDao
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
+    private lateinit var dao: ShoppingDao
 
     @Before
-    fun setup(){
-        database= Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
-        dao=database.shoppingDao()
+    fun setup() {
+        hiltRule.inject()
+        dao = database.shoppingDao()
     }
 
     @After
-    fun teardown(){
+    fun teardown() {
         database.close()
     }
 
     @Test
-    fun insertShoppingItem()= runBlockingTest {
-        val shoppingItem=ShoppingItem("name",1,1f,"url",1)
+    fun insertShoppingItem() = runBlockingTest {
+
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
         dao.insertShoppingItem(shoppingItem)
 
-        val allShoppingItem=dao.observeAllShoppingItems().getOrAwaitValue()
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
 
-        assertThat(allShoppingItem).contains(shoppingItem)
+        assertThat(allShoppingItems).contains(shoppingItem)
     }
 
     @Test
-    fun delelteShoppingItem()= runBlocking {
-        val shoppingItem=ShoppingItem("name",1,1f,"url",1)
+    fun deleteShoppingItem() = runBlockingTest {
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
         dao.insertShoppingItem(shoppingItem)
         dao.deleteShoppingItem(shoppingItem)
 
-        val allShoppingItem=dao.observeAllShoppingItems().getOrAwaitValue()
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
 
-        assertThat(allShoppingItem).doesNotContain(shoppingItem)
+        assertThat(allShoppingItems).doesNotContain(shoppingItem)
     }
 
     @Test
-    fun observeTotalPriceSum()= runBlockingTest {
-        val shoppingItem1=ShoppingItem("name",2,10f,"url",1)
-        val shoppingItem2=ShoppingItem("name",4,5.5f,"url",2)
-        val shoppingItem3=ShoppingItem("name",0,100f,"url",3)
-
+    fun observeTotalPriceSum() = runBlockingTest {
+        val shoppingItem1 = ShoppingItem("name", 2, 10f, "url", id = 1)
+        val shoppingItem2 = ShoppingItem("name", 4, 5.5f, "url", id = 2)
+        val shoppingItem3 = ShoppingItem("name", 0, 100f, "url", id = 3)
         dao.insertShoppingItem(shoppingItem1)
         dao.insertShoppingItem(shoppingItem2)
         dao.insertShoppingItem(shoppingItem3)
 
-        val totalPriceSum=dao.observeTotalPrice().getOrAwaitValue()
+        val totalPriceSum = dao.observeTotalPrice().getOrAwaitValue()
 
-        assertThat(totalPriceSum).isEqualTo(2*10f+4*5.5f)
-
+        assertThat(totalPriceSum).isEqualTo(2 * 10f + 4 * 5.5f)
     }
-
 }
