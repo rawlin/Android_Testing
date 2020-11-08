dc3c8606f4a8c05412414dac92ef47dc2913578d
rawlin <rawlincrasto@gmail.com>
2020-11-08 18:42:11 +0530

    Adding item test

--- ShoppingListTestingYT/app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingFragmentFactory.kt
+++ ShoppingListTestingYT/app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingFragmentFactory.kt
@@ -3,16 +3,19 @@ package com.androiddevs.shoppinglisttestingyt.ui
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.androiddevs.shoppinglisttestingyt.adapters.ImageAdapter
+import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ShoppingFragmentFactory
 @Inject
 constructor(
-    private val imageAdapter: ImageAdapter
+    private val imageAdapter: ImageAdapter,
+    private val glide:RequestManager
 ):FragmentFactory() {
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when(className){
             ImagePickFragment::class.java.name->ImagePickFragment(imageAdapter)
+            AddShoppingItemFragment::class.java.name->AddShoppingItemFragment(glide)
             else->super.instantiate(classLoader, className)
         }
     }
